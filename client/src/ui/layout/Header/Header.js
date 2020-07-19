a9dbadc0b143cd809708c2458f7bfa24cb6d68d2
damianpugliese <damian.pugliese@gmail>
2020-07-18 23:52:54 -0300

    responsive navbar

--- client/src/ui/layout/Header/Header.js
+++ client/src/ui/layout/Header/Header.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './hamburgers.css';
 import logo from '../../../assets/logo.png';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
@@ -33,18 +34,64 @@ const useStyles = makeStyles((theme) => ({
     },
     navBar: {
         display: 'inline-flex',
+        [theme.breakpoints.down('xs')]: {
+            display: 'none'
+        }
     },
     links: {
         textDecoration: 'none',
         color: '#000',
         paddingLeft: '25px',
         fontSize: '1.2em',
+    },
+    navBarResponsive: {
+        display: 'none',
+        [theme.breakpoints.down('xs')]: {
+            display: 'flex',
+            top: 55,
+            paddingTop: 24,
+            paddingLeft: 16,
+            flexDirection: 'column',
+            position: 'fixed',
+            width: '100%',
+            height: 'calc(100vh - 55px)',
+            justifyContent: 'flex-start',
+            background: '#fff',
+            right: '-100%',
+            zIndex: 3,
+            transition: 'all 500ms ease-in-out',
+        }
+    },
+    isOpen: {
+        extend: 'navBarResponsive',
+        right: 0,
+        zIndex: 4
+    },
+    hamburguer: {
+        display: 'none',
+        [theme.breakpoints.down('xs')]: {
+            display: 'flex',
+        }
+    },
+    linksResponsive: {
+        textDecoration: 'none',
+        color: '#000',
+        padding: '10px 0',
+        fontSize: '1.3em',
+        fontWeight: 'bold'
     }
 }));
 
 const Header = () => {
 
     const classes = useStyles();
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleNavResponsive = () => {
+        setIsVisible(!isVisible);
+        document.body.classList.toggle('scroll');
+    }
 
     return (
         <header className={classes.header}>
@@ -61,6 +108,17 @@ const Header = () => {
                     <Link to="/events" className={classes.links}>Events</Link>
                     <Link to="/bookings" className={classes.links}>Bookings</Link>
                 </nav>
+                <nav className={`${classes.navBarResponsive} ${isVisible ? classes.isOpen : ''}`}>
+                    <Link to="/login" className={classes.linksResponsive} onClick={toggleNavResponsive}>Login</Link>
+                    <Link to="/signup" className={classes.linksResponsive} onClick={toggleNavResponsive}>Signup</Link>
+                    <Link to="/events" className={classes.linksResponsive} onClick={toggleNavResponsive}>Events</Link>
+                    <Link to="/bookings" className={classes.linksResponsive} onClick={toggleNavResponsive}>Bookings</Link>
+                </nav>
+                <button className={`${classes.hamburguer} hamburger hamburger--squeeze ${!isVisible ? "" : "is-active"}`} type="button" onClick={toggleNavResponsive}>
+                    <span className="hamburger-box">
+                        <span className="hamburger-inner"></span>
+                    </span>
+                </button>
             </Container>
         </header>
     )
